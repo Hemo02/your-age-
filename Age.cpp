45bd8650eb14422aaee4ec2d33f8b4479c3a9fb5
Ibrahim Fouad <100357342+Hemo02@users.noreply.github.com>
2022-09-01 01:29:12 +0200

    Add files via upload

---
+++ Age.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	int year = 2022;
+	int born;
+	cout << "enter you date born" << endl;
+	cin  >> born;
+
+	int age = year - born;
+
+	cout << "this your boirn :" << age;
+
+}
